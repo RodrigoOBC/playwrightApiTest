a8a982c043e0d7cbe6f955ff20236d09f1985816
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-01-31 11:43:52 -0300

    Add tests for retrieving users by ID and schema validation for user data

--- tests/user/STATUS200.spec.js
+++ tests/user/STATUS200.spec.js
@@ -1,25 +1,33 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 import Ajv from 'ajv';
+
 const CT01 = require('./resources_schemas/CT01.schema.json')
+const CT02 = require('./resources_schemas/CT02.schema.json')
 const ajv = new Ajv();
 
+const Id_list = [1, 2, 5, 8];
+
 test('CT01 - get all User', async ({ request }) => {
-  
+
   const response = await request.get('/users');
   const body = await response.json();
-  
+
   expect(response.status()).toBe(200);
   expect(await ajv.validate(CT01, body)).toBeTruthy();
 
 });
 
-test('CT02 - get User by ID', async ({ request }) => {
 
-  const response = await request.get('/users/1');
-  const body = await response.json();
-  
-  expect(response.status()).toBe(200);
-  expect(body.id).toBe(1);
-  
-})
+for (let id of Id_list) {
+  test(`CT02 - get User by ID ${id}`, async ({ request }) => {
+
+    const response = await request.get(`/users/${id}`);
+    const body = await response.json();
+
+    expect(response.status()).toBe(200);
+    expect(body.id).toBe(id);
+    expect(await ajv.validate(CT02, body)).toBeTruthy();
+  })
+}
+
