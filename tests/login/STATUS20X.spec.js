113aa919d1f01616bc534000039706733402b7f7
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-02-17 11:39:49 -0300

    teste

--- tests/login/STATUS20X.spec.js
+++ tests/login/STATUS20X.spec.js
@@ -4,7 +4,7 @@ import Ajv from 'ajv';
 const CT01 = require('./resources_schemas/CT01.schema.json')
 const ajv = new Ajv();
 
-test.only('POST - /login - CT01  - get Token with Suscess', async ({ request }) => {
+test('POST - /login - CT01  - get Token with Suscess', async ({ request }) => {
 
     const payloadRequest = JSON.stringify({
         username: process.env.USERNAMEAPI,
